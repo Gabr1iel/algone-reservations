35eb87422411684a678cd28c3cd31f0750374948
Bc. Jitka Čabelická <jitka.cabelicka@gmail.com>
2026-03-09 12:11:50 +0100

    Improve reservation API responses using ResponseEntity

--- reservations-server/src/main/java/com/algone/reservations/controller/ReservationController.java
+++ reservations-server/src/main/java/com/algone/reservations/controller/ReservationController.java
@@ -2,6 +2,7 @@ package com.algone.reservations.controller;
 
 import com.algone.reservations.entity.Reservation;
 import com.algone.reservations.service.ReservationService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,27 +18,33 @@ public class ReservationController {
     }
 
     @PostMapping
-    public Reservation createReservation(@RequestBody Reservation reservation) {
-        return reservationService.createReservation(reservation);
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
+        Reservation created = reservationService.createReservation(reservation);
+        return ResponseEntity.ok(created);
     }
 
     @GetMapping
-    public List<Reservation> getAllReservations() {
-        return reservationService.getAllReservations();
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        return ResponseEntity.ok(reservationService.getAllReservations());
     }
 
     @GetMapping("/{id}")
-    public Reservation getReservationById(@PathVariable Long id) {
-        return reservationService.getReservationById(id);
+    public ResponseEntity<Reservation> getReservationById(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.getReservationById(id));
     }
 
     @PostMapping("/{id}/confirm")
-    public Reservation confirmReservation(@PathVariable Long id) {
-        return reservationService.confirmReservation(id);
+    public ResponseEntity<Reservation> confirmReservation(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.confirmReservation(id));
+    }
+
+    @PostMapping("/{id}/complete")
+    public ResponseEntity<Reservation> completeReservation(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.completeReservation(id));
     }
 
     @PostMapping("/{id}/cancel")
-    public Reservation cancelReservation(@PathVariable Long id) {
-        return reservationService.cancelReservation(id);
+    public ResponseEntity<Reservation> cancelReservation(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.cancelReservation(id));
     }
 }
