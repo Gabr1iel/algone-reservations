838a2d5aec6d1236a326b5480259484ce98d66f7
Bc. Jitka Čabelická <jitka.cabelicka@gmail.com>
2026-03-09 11:42:28 +0100

    Update reservations to use user_id instead of guest_id

--- reservations-server/src/main/resources/db/migration/V7__create_reservations_table.sql
+++ reservations-server/src/main/resources/db/migration/V7__create_reservations_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS reservations (
                                             id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                             hotel_id BIGINT NOT NULL,
                                             room_id BIGINT NOT NULL,
-                                            guest_id BIGINT NOT NULL,
+                                            user_id BIGINT NOT NULL,
                                             from_date DATE NOT NULL,
                                             to_date DATE NOT NULL,
                                             status VARCHAR(20) NOT NULL DEFAULT 'CREATED',
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS reservations (
 
     INDEX idx_reservations_hotel_id (hotel_id),
     INDEX idx_reservations_room_id (room_id),
-    INDEX idx_reservations_guest_id (guest_id),
+    INDEX idx_reservations_user_id (user_id),
     INDEX idx_reservations_status (status),
     INDEX idx_reservations_dates (from_date, to_date)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
