ad4d9f947b03539d8433788726d47d1029fd0e73
jitkacabelicka-png <jitka.cabelicka@gmail.com>
2026-03-16 17:20:11 +0100

    Create CustomUserDetails for user authentication
    Implemented CustomUserDetails class to represent user details in Spring Security context.

--- reservations-server/src/main/java/com/algone/reservations/security/CustomUserDetails.java
+++ reservations-server/src/main/java/com/algone/reservations/security/CustomUserDetails.java
@@ -1,4 +1,54 @@
 package com.algone.reservations.security;
 
-public class CustomUserDetails {
+import com.algone.reservations.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.password = user.getPasswordHash();
+        this.authorities = List.of(new SimpleGrantedAuthority("ROLE_" +  user.getRole().name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
