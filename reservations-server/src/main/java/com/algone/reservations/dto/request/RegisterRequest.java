e3d453e2ca0cf33926b16315b4a09946160e23e9
jitkacabelicka-png <jitka.cabelicka@gmail.com>
2026-03-16 17:27:06 +0100

    Add validation annotations to RegisterRequest fields

--- reservations-server/src/main/java/com/algone/reservations/dto/request/RegisterRequest.java
+++ reservations-server/src/main/java/com/algone/reservations/dto/request/RegisterRequest.java
@@ -1,4 +1,27 @@
 package com.algone.reservations.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RegisterRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Heslo musí mít aspoň 8 znaků!")
+    private String password;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    private String phone;
 }
