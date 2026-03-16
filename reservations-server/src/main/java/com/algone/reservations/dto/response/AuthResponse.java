e85233783008913c32275ab2c7a42c12091ee8fd
jitkacabelicka-png <jitka.cabelicka@gmail.com>
2026-03-16 17:28:16 +0100

    Enhance AuthResponse with additional user fields
    Added fields for token, email, first name, last name, and role.

--- reservations-server/src/main/java/com/algone/reservations/dto/response/AuthResponse.java
+++ reservations-server/src/main/java/com/algone/reservations/dto/response/AuthResponse.java
@@ -1,4 +1,14 @@
 package com.algone.reservations.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AuthResponse {
+    private String token;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
 }
