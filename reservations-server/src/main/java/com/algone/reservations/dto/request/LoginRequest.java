1d92f59fd601b23dfca906f7682440e8b2aad19a
jitkacabelicka-png <jitka.cabelicka@gmail.com>
2026-03-16 17:26:12 +0100

    Add validation annotations to LoginRequest fields

--- reservations-server/src/main/java/com/algone/reservations/dto/request/LoginRequest.java
+++ reservations-server/src/main/java/com/algone/reservations/dto/request/LoginRequest.java
@@ -1,4 +1,17 @@
 package com.algone.reservations.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
 }
